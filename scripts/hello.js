2fd169c1fd1acc43cdeadbba6d3d0dcf788bd783
vimalindhu <122624574+vimalindhu@users.noreply.github.com>
2023-03-23 20:21:12 -0400

    created the form

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,1 +1,15 @@
- Hello world 
+function printhealine(message, size) {
+    var headline = "<h" + size + ">";
+    var headlineEnd = "<h" + size + ">";
+    document.write(headline + message + headlineEnd);
+}
+
+function calculateSalesTax(subtotal, percent) {
+    percent = percent / 100;
+    var total = subtotal * percent;
+    document.write(total)
+}
+
+
+
+document.write("Hello world!");
